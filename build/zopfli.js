4c931667a6f455fb48b855e6213b70c51b97aa60
Lachlan Collins <lockyc@gmail.com>
2013-07-05 13:38:20 +1000

    zopfli changes

--- build/zopfli.js
+++ build/zopfli.js
@@ -4,47 +4,43 @@ var fs = require('fs');
 var execSync = require('execSync');
 
 execSync.exec('git pull');
+execSync.exec('git checkout zopfli');
+execSync.exec('git merge master');
+glob("../ajax/libs/**/package.json", function (error, matches) {
+    matches.forEach(function(element){
+        var package = JSON.parse(fs.readFileSync(element, 'utf8'));
+        package.assets = Array();
+        var versions = glob.sync("../ajax/libs/"+package.name+"/!(package.json)");
+        versions.forEach(function(version) {
+            var temp = Object();
+            temp.files = glob.sync(version + "/**/*.*");
 
-var exec=require('child_process').exec;
-exec('git checkout zopfli',function(err,stdout,stderr){
-    execSync.exec('git merge master');
-    glob("../ajax/libs/**/package.json", function (error, matches) {
-        matches.forEach(function(element){
-            var package = JSON.parse(fs.readFileSync(element, 'utf8'));
-            package.assets = Array();
-            var versions = glob.sync("../ajax/libs/"+package.name+"/!(package.json)");
-            versions.forEach(function(version) {
-                var temp = Object();
-                temp.files = glob.sync(version + "/**/*.*");
-
-                for (var i = 0; i < temp.files.length; i++){
-                    var regex = /(?:^.+\/)?(.+?)?\.((?:js)|(?:css))([\.-](?:gz))?$/ig;
-                    var result = regex.exec(temp.files[i]);
-                    //result[0] Original Input
-                    //result[1] Filename
-                    //result[2] js|css
-                    //result[3] gz
+            for (var i = 0; i < temp.files.length; i++){
+                var regex = /(?:^.+\/)?(.+?)?\.((?:js)|(?:css))([\.-](?:gz))?$/ig;
+                var result = regex.exec(temp.files[i]);
+                //result[0] Original Input
+                //result[1] Filename
+                //result[2] js|css
+                //result[3] gz
 
 
-                    if (result == null) {
-                        continue;
-                    }
+                if (result == null) {
+                    continue;
+                }
 
-                    if (typeof result[3] == "undefined") { //
-                        if (!fs.exists(temp.files[i] + ".gz")) {
-                            console.log('zopfli', temp.files[i])
-                            execSync.exec('zopfli ' + temp.files[i]);
-                            console.log('zopfli ended')
-                        }
+                if (typeof result[3] == "undefined") { //
+                    if (!fs.exists(temp.files[i] + ".gz")) {
+                        console.log('zopfli', temp.files[i])
+                        execSync.exec('zopfli ' + temp.files[i]);
+                        console.log('zopfli ended')
                     }
                 }
-            });
+            }
         });
     });
+});
 
-    execSync.exec('git add ../.');
-    execSync.exec('git commit -am "zopfli"');
-    execSync.exec('git push');
-    execSync.exec('git checkout master');
-
-})
+execSync.exec('git add ../.');
+execSync.exec('git commit -am "zopfli"');
+execSync.exec('git push');
+execSync.exec('git checkout master');
