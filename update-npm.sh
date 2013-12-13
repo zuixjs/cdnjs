239615078b1e137b37d6c85fbbb235269adb1c56
Thomas Davis <thomasalwyndavis@gmail.com>
2013-12-13 13:58:35 +1000

    Updated packages via NPM auto-update.

--- update-npm.sh
+++ update-npm.sh
@@ -2,17 +2,17 @@
 # Exit if any errors
 set -e
 
-echo Getting latest website changes...
+echo Getting latest libraries
 git pull
 
 echo npm install for good mesure
 npm install
 
-echo Re-generating website...
+echo Starting npm script
 node npm-auto-update.js
 
-echo Uploading website...
+echo Pushing new versions
 git add .
-git commit -am "Updated packages."
+git commit -am "Updated packages via NPM auto-update."
 git push
 
