502b69175de6dd6dbb685c3e530d6ae3f5eec95b
Thomas Davis <thomasalwyndavis@gmail.com>
2013-12-13 12:37:49 +1000

    update shell script

---
+++ update-npm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Exit if any errors
+set -e
+
+echo Getting latest website changes...
+git pull
+
+echo npm install for good mesure
+npm install
+
+echo Re-generating website...
+node npm-auto-update.js
+
+echo Uploading website...
+git add .
+git commit -am "Updated packages."
+git push
+
