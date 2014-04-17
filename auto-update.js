4fbed1ede92d6c2b6d2bab07644808ccf7f7c7cd
Misha Koryak <koryak@gmail.com>
2014-04-17 18:24:24 -0400

    refactor code to be more testable

--- auto-update.js
+++ auto-update.js
@@ -64,6 +64,92 @@ var isValidFileMap = function(pkg){
     return false
 };
 
+var error = function(msg, name){
+    var err = new Error(msg);
+    err.name = name;
+    console.log(msg);
+    hipchat.message('red', msg);
+    return err;
+}
+error.PKG_NAME = 'BadPackageName'
+error.FILE_PATH = 'BadFilePath'
+
+
+var isAllowedPathFn = function(libPath){ //is path within the lib dir? if not, they shouldnt be writing/reading there
+    return function(){
+        var paths = 1 <= arguments.length ? [].slice.call(arguments, 0) : [];
+        var re = new RegExp("^"+libPath)
+        return _.every(paths, function(p) {
+            p = path.normalize(p);
+            return p.match(re);
+        });
+    }
+};
+
+var processNewVersion = function(pkg, libPath, folderName){
+    var isAllowedPath = isAllowedPathFn(libPath);
+
+    var newPkg = parse(path.join(libPath, folderName, 'package.json'));
+    if(isValidFileMap(newPkg)){
+        pkg.npmFileMap = newPkg.npmFileMap;
+    }
+    var npmFileMap = pkg.npmFileMap;
+    var errors = [];
+
+    _.each(npmFileMap, function(fileSpec) {
+        var basePath = fileSpec.basePath || "";
+
+        _.each(fileSpec.files, function(file) {
+            var libContentsPath = path.normalize(path.join(libPath, folderName, basePath, file));
+            if(!isAllowedPath(libContentsPath)){
+                errors.push(error(pkg.npmName+" contains a malicious file path: "+libContentsPath, error.FILE_PATH));
+                return
+            }
+            var files = glob.sync(libContentsPath);
+
+            _.each(files, function(extractFilePath) {
+                if(extractFilePath.match(/(dependencies|\.zip\s*$)/i)) return;
+
+                var replacePath = path.normalize(path.join(folderName, basePath));
+                var actualPath = extractFilePath.replace(replacePath, "");
+                if(!isAllowedPath(extractFilePath, actualPath)){
+                    errors.push(error(pkg.npmName+" contains a malicious file path: "+extractFilePath+' or '+actualPath, error.FILE_PATH));
+                    return;
+                }
+                fs.renameSync(extractFilePath, actualPath);
+            });
+        });
+    });
+    return errors;
+}
+
+var updateLibraryVersion = function(pkg, tarballUrl, version, cb) {
+    if(~pkg.name.indexOf("..")){
+        return cb(error(pkg.npmName+" has a malicious package name:"+ pkg.name, error.PKG_NAME));
+    }
+    var libPath = path.normalize(path.join(__dirname, 'ajax', 'libs', pkg.name, version));
+
+
+    if(!fs.existsSync(libPath)) {
+        fs.mkdirSync(libPath);
+        var url = tarballUrl;
+        var downloadFile = libPath + '/dist.tar.gz';
+        tarball.extractTarballDownload(url , downloadFile, libPath, {}, function(err, result) {
+
+            fs.unlinkSync(downloadFile);
+            var folderName = fs.readdirSync(libPath)[0];
+            processNewVersion(pkg, libPath, folderName);
+
+            fs.removeSync(path.join(libPath, folderName));
+
+            newVersionCount++;
+            console.log("Do not have version", version, "of", pkg.npmName);
+            cb()
+        });
+    } else {
+        cb()
+    }
+};
 var updateLibrary = function (pkg, callback) {
     if(!isValidFileMap(pkg)){
         console.log(pkg.npmName+" has a malicious npmFileMap");
@@ -72,75 +158,17 @@ var updateLibrary = function (pkg, callback) {
     }
     console.log('Checking versions for ' + pkg.npmName);
     request.get('http://registry.npmjs.org/' + pkg.npmName, function(result) {
-        _.each(result.body.versions, function(data, version) {
-            if(~pkg.name.indexOf("..")){
-                console.log(pkg.npmName+" has a malicious package name:", pkg.name);
-                hipchat.message('red', pkg.npmName+" has a malicious package name: "+pkg.name);
-                return;
-            }
-            var libPath = path.normalize(path.join(__dirname, 'ajax', 'libs', pkg.name, version));
+        async.eachLimit(_.pairs(result.body.versions), 5, function(p, cb){ //extract 5 at a time
+            var data = p[1];
+            var version = p[0];
+            updateLibraryVersion(pkg, data.dist.tarball, version, cb)
+        }, function(err){
+            var npmVersion = result.body['dist-tags'] && result.body['dist-tags'].latest || 0;
+            pkg.version = npmVersion;
+            fs.writeFileSync('ajax/libs/' + pkg.name + '/package.json', JSON.stringify(pkg, null, 2), 'utf8');
 
-            var isAllowedPath = function(){ //is path within the lib dir? if not, they shouldnt be writing/reading there
-                var paths = 1 <= arguments.length ? [].slice.call(arguments, 0) : [];
-                var re = new RegExp("^"+libPath)
-                return _.every(paths, function(p) {
-                    p = path.normalize(p);
-                    return p.match(re);
-                });
-            };
-
-            if(!fs.existsSync(libPath)) {
-                console.log('dont have', version);
-                fs.mkdirSync(libPath);
-                var url = data.dist.tarball;
-                var downloadFile = libPath + '/dist.tar.gz';
-                tarball.extractTarballDownload(url , downloadFile, libPath, {}, function(err, result) {
-
-                    fs.unlinkSync(downloadFile);
-                    var folderName = fs.readdirSync(libPath)[0];
-                    var newPkg = parse(path.join(libPath, folderName, 'package.json'));
-                    if(isValidFileMap(newPkg)){
-                        pkg.npmFileMap = newPkg.npmFileMap;
-                    }
-                    var npmFileMap = pkg.npmFileMap;
-
-                    _.each(npmFileMap, function(fileSpec) {
-                        var basePath = fileSpec.basePath || "";
-
-                        _.each(fileSpec.files, function(file) {
-                            var libContentsPath = path.normalize(path.join(libPath, folderName, basePath, file));
-                            if(!isAllowedPath(libContentsPath)){
-                                console.log(pkg.npmName+" contains a malicious file path: ", libContentsPath);
-                                hipchat.message('red', pkg.npmName+" contains a malicious file path: "+libContentsPath);
-                            }
-                            var files = glob.sync(libContentsPath);
-
-                            _.each(files, function(extractFilePath) {
-                                if(extractFilePath.match(/(dependencies|\.zip\s*$)/i)) return;
-
-                                var replacePath = path.normalize(path.join(folderName, basePath));
-                                var actualPath = extractFilePath.replace(replacePath, "");
-                                if(!isAllowedPath(extractFilePath, actualPath)){
-                                    console.log(pkg.npmName+" contains a malicious file path: ", extractFilePath, actualPath);
-                                    hipchat.message('red', pkg.npmName+" contains a malicious file path: "+extractFilePath+' or '+actualPath);
-                                    return;
-                                }
-                                fs.renameSync(extractFilePath, actualPath);
-                            });
-                        });
-                    });
-
-                    fs.removeSync(path + '/' + folderName);
-                });
-                newVersionCount++;
-                console.log("Do not have version", version, "of", pkg.npmName);
-            }
+            callback(null);
         });
-        var npmVersion = result.body['dist-tags'] && result.body['dist-tags'].latest || 0;
-        pkg.version = npmVersion;
-        fs.writeFileSync('ajax/libs/' + pkg.name + '/package.json', JSON.stringify(pkg, null, 2), 'utf8');
-
-        callback(null);
     });
 }
 
---
+++ test/auto-update-test.js
@@ -0,0 +1,8 @@
+var assert = require("assert"),
+    path = require("path"),
+    fs = require("fs"),
+    glob = require("glob"),
+    vows = require("vows-si"),
+    jsv = require("JSV").JSV.createEnvironment();
+
+//TODO
