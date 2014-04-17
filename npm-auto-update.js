d6167d968c2addd3790ffa9681c5ec697e93ef77
Misha Koryak <koryak@gmail.com>
2014-04-16 23:53:41 -0400

    - use npmFileMap from npm tarball if present - filter out malicious paths from filemap - clean up some code

--- npm-auto-update.js
+++ npm-auto-update.js
@@ -28,37 +28,79 @@ var parse = function (json_file, ignore_missing, ignore_parse_fail) {
     }
 }
 
+var isValidFileMap = function(pkg){
+    var isValidPath = function(p){
+        return p !== null && !p.match(/([\//]\.\.[\//])/); //don't allow parent dir access
+    };
+
+    if(pkg && pkg.npmFileMap){
+        return _.every(pkg.npmFileMap, function(fileSpec){
+           if(isValidPath(fileSpec.basePath || "")){
+               return _.every(fileSpec.files, isValidPath);
+           }
+           return false;
+        });
+    }
+    return false
+};
+
 var updateLibrary = function (pkg, callback) {
+    if(!isValidFileMap(pkg)){
+        console.log(pkg.npmName+" has a malicious npmFileMap, skipping!");
+        return callback(null);
+    }
     console.log('Checking versions for ' + pkg.npmName);
     request.get('http://registry.npmjs.org/' + pkg.npmName, function(result) {
         _.each(result.body.versions, function(data, version) {
-            var path = './ajax/libs/' + pkg.name + '/' + version;
-            console.log(path);
-            if(!fs.existsSync(path)) {
-                console.log('dont have', version);
-                fs.mkdirSync(path);
-                var url = data.dist.tarball;
-                var download_file = path + '/dist.tar.gz';
-                tarball.extractTarballDownload(url , download_file, path, {}, function(err, result) {
-                    console.log('this is happening sync');
-                    fs.unlinkSync(download_file);
-                    var folderName = fs.readdirSync(path)[0];
+            if(~pkg.name.indexOf("..")){
+                console.log(pkg.npmName+" has a malicious package name, skipping! ", pkg.name);
+                return;
+            }
+            var libPath = path.normalize(path.join(__dirname, 'ajax', 'libs', pkg.name, version));
 
+            var isAllowedPath = function(){ //is path within the lib dir? if not, they shouldnt be writing/reading there
+                var paths = 1 <= arguments.length ? [].slice.call(arguments, 0) : [];
+                var re = new RegExp("^"+libPath)
+                return _.every(paths, function(p) {
+                    p = path.normalize(p);
+                    return p.match(re);
+                });
+            };
+
+            if(!fs.existsSync(libPath)) {
+                console.log('dont have', version);
+                fs.mkdirSync(libPath);
+                var url = data.dist.tarball;
+                var downloadFile = libPath + '/dist.tar.gz';
+                tarball.extractTarballDownload(url , downloadFile, libPath, {}, function(err, result) {
+
+                    fs.unlinkSync(downloadFile);
+                    var folderName = fs.readdirSync(libPath)[0];
+                    var newPkg = parse(path.join(libPath, folderName, 'package.json'));
+                    if(isValidFileMap(newPkg)){
+                        pkg.npmFileMap = newPkg.npmFileMap;
+                    }
                     var npmFileMap = pkg.npmFileMap;
 
                     _.each(npmFileMap, function(fileSpec) {
                         var basePath = fileSpec.basePath || "";
 
                         _.each(fileSpec.files, function(file) {
-                            var extractPath = basePath + "/" + file;
-                            var files = glob.sync(path + "/" + folderName + "/" + basePath + "/" + file);
+                            var libContentsPath = path.normalize(path.join(libPath, folderName, basePath, file));
+                            if(!isAllowedPath(libContentsPath)){
+                                console.log(pkg.npmName+" contains a malicious file path, skipping: ", libContentsPath);
+                            }
+                            var files = glob.sync(libContentsPath);
 
                             _.each(files, function(extractFilePath) {
-                                if(extractFilePath.slice(-4) == ".zip") return;
-                                if(extractFilePath.indexOf("dependencies") !== -1) return;
-                                var replacePath = folderName + "/" + basePath + "/";
-                                replacePath = replacePath.replace(/\/\//g, "/");
+                                if(extractFilePath.match(/(dependencies|\.zip\s*$)/i)) return;
+
+                                var replacePath = path.normalize(path.join(folderName, basePath));
                                 var actualPath = extractFilePath.replace(replacePath, "");
+                                if(!isAllowedPath(extractFilePath, actualPath)){
+                                    console.log(pkg.npmName+" contains a malicious file path, skipping: ", extractFilePath, actualPath);
+                                    return;
+                                }
                                 fs.renameSync(extractFilePath, actualPath);
                             });
                         });
@@ -73,26 +115,21 @@ var updateLibrary = function (pkg, callback) {
         pkg.version = npmVersion;
         fs.writeFileSync('ajax/libs/' + pkg.name + '/package.json', JSON.stringify(pkg, null, 2), 'utf8');
 
-        callback(null, pkg['npm-name']);
+        callback(null);
     });
 }
 
 console.log('Looking for npm enabled libraries...');
 
 // load up those files
-var packages = glob.sync("./ajax/libs/**/package.json");
+var packages = glob.sync("./ajax/libs/*/package.json");
 packages = _(packages).map(function (pkg) {
     var parsedPkg = parse(pkg);
     return parsedPkg.npmName ? parsedPkg : null;
 }).compact().value();
 
 console.log('Found ' + packages.length + ' npm enabled libraries');
-var libraryUpdates = [];
-_.each(packages, function(pkg) {
-    libraryUpdates.push(function (callback) {
-      updateLibrary(pkg, callback);
-    });;
-});
-async.series(libraryUpdates, function(err, results) {
+
+async.eachSeries(packages, updateLibrary, function(err) {
   console.log('Script completed');
 });
