58d83e358a276ce464e1ee8b3b6bd1e720ee88ca
Misha Koryak <koryak@gmail.com>
2014-04-18 17:24:59 -0400

    more cleanup for tests

--- auto-update.js
+++ auto-update.js
@@ -1,4 +1,12 @@
-var Hipchat = require('node-hipchat');
+var Hipchat = require('node-hipchat'),
+    path = require("path"),
+    fs = require("fs-extra"),
+    glob = require("glob"),
+    _ = require('lodash'),
+    request = require("superagent"),
+    async = require("async"),
+    tarball = require('tarball-extract'),
+    mkdirp = require('mkdirp');
 
 var HC = new Hipchat(process.env.HIPCHAT);
 var hipchat = {
@@ -17,14 +25,7 @@ var hipchat = {
     }
   }
 };
-var path = require("path"),
-    fs = require("fs-extra"),
-    glob = require("glob"),
-    _ = require('lodash'),
-    request = require("superagent"),
-    async = require("async"),
-    tarball = require('tarball-extract'),
-    mkdirp = require('mkdirp');
+
 hipchat.message('gray', 'Auto Update Started');
 var newVersionCount = 0;
 var parse = function (json_file, ignore_missing, ignore_parse_fail) {
@@ -48,9 +49,18 @@ var parse = function (json_file, ignore_missing, ignore_parse_fail) {
     }
 }
 
+/**
+ * Check if an npmFileMap object contains any path which are not normalized, and thus could allow access to parent dirs
+ * @param pkg
+ * @returns {*}
+ */
 var isValidFileMap = function(pkg){
     var isValidPath = function(p){
-        return p !== null && !p.match(/([\//]\.\.[\//])/); //don't allow parent dir access
+        if(p !== null){ //don't allow parent dir access, or tricky paths
+            p = p.replace(/\/+/g, '/'); //dont penalize for consequtive path seperators
+            return p === path.normalize(p);
+        }
+        return false
     };
 
     if(pkg && pkg.npmFileMap){
@@ -74,8 +84,12 @@ var error = function(msg, name){
 error.PKG_NAME = 'BadPackageName'
 error.FILE_PATH = 'BadFilePath'
 
-
+/**
+* returns a fucntion that takes N args, where each arg is a path that must not outside of libPath.
+ * returns true if all paths are within libPath, else false
+*/
 var isAllowedPathFn = function(libPath){ //is path within the lib dir? if not, they shouldnt be writing/reading there
+    libPath = path.normalize(libPath);
     return function(){
         var paths = 1 <= arguments.length ? [].slice.call(arguments, 0) : [];
         var re = new RegExp("^"+libPath)
@@ -86,10 +100,18 @@ var isAllowedPathFn = function(libPath){ //is path within the lib dir? if not, t
     }
 };
 
-var processNewVersion = function(pkg, libPath, folderName){
+
+/**
+ * Attempt to update the npmFileMap from extracted package.json, then using npmFileMap move required files to libPath/../
+ * If the npmFileMap tries to modify files outside of libPath, dont let it!
+ * @param pkg
+ * @param libPath = root folder for extracted lib
+ * @returns {Array} = array of security related errors triggered during operation.
+ */
+var processNewVersion = function(pkg, libPath){
     var isAllowedPath = isAllowedPathFn(libPath);
 
-    var newPkg = parse(path.join(libPath, folderName, 'package.json'));
+    var newPkg = parse(path.join(libPath, 'package.json'));
     if(isValidFileMap(newPkg)){
         pkg.npmFileMap = newPkg.npmFileMap;
     }
@@ -100,7 +122,7 @@ var processNewVersion = function(pkg, libPath, folderName){
         var basePath = fileSpec.basePath || "";
 
         _.each(fileSpec.files, function(file) {
-            var libContentsPath = path.normalize(path.join(libPath, folderName, basePath, file));
+            var libContentsPath = path.normalize(path.join(libPath, basePath, file));
             if(!isAllowedPath(libContentsPath)){
                 errors.push(error(pkg.npmName+" contains a malicious file path: "+libContentsPath, error.FILE_PATH));
                 return
@@ -110,7 +132,7 @@ var processNewVersion = function(pkg, libPath, folderName){
             _.each(files, function(extractFilePath) {
                 if(extractFilePath.match(/(dependencies|\.zip\s*$)/i)) return;
 
-                var replacePath = path.normalize(path.join(folderName, basePath));
+                var replacePath = path.normalize(path.join('package', basePath));
                 var actualPath = extractFilePath.replace(replacePath, "");
                 if(!isAllowedPath(extractFilePath, actualPath)){
                     errors.push(error(pkg.npmName+" contains a malicious file path: "+extractFilePath+' or '+actualPath, error.FILE_PATH));
@@ -123,6 +145,14 @@ var processNewVersion = function(pkg, libPath, folderName){
     return errors;
 }
 
+/**
+ * download and extract a tarball for a single npm version, get the files in npmFileMap and delete the rest
+ * @param pkg
+ * @param tarballUrl
+ * @param version
+ * @param cb
+ * @returns {*}
+ */
 var updateLibraryVersion = function(pkg, tarballUrl, version, cb) {
     if(~pkg.name.indexOf("..")){
         return cb(error(pkg.npmName+" has a malicious package name:"+ pkg.name, error.PKG_NAME));
@@ -135,13 +165,10 @@ var updateLibraryVersion = function(pkg, tarballUrl, version, cb) {
         var url = tarballUrl;
         var downloadFile = libPath + '/dist.tar.gz';
         tarball.extractTarballDownload(url , downloadFile, libPath, {}, function(err, result) {
-
             fs.unlinkSync(downloadFile);
-            var folderName = fs.readdirSync(libPath)[0];
-            processNewVersion(pkg, libPath, folderName);
-
-            fs.removeSync(path.join(libPath, folderName));
-
+            var extractPath = path.join(libPath, 'package');
+            processNewVersion(pkg, extractPath);
+            fs.removeSync(extractPath);
             newVersionCount++;
             console.log("Do not have version", version, "of", pkg.npmName);
             cb()
@@ -150,11 +177,18 @@ var updateLibraryVersion = function(pkg, tarballUrl, version, cb) {
         cb()
     }
 };
-var updateLibrary = function (pkg, callback) {
+
+/**
+ * grab all versions of a lib that has an 'npmFileMap' and 'npmName' in its package.json
+ * @param pkg
+ * @param tarballUrl
+ * @param cb
+ */
+var updateLibrary = function (pkg, cb) {
     if(!isValidFileMap(pkg)){
         console.log(pkg.npmName+" has a malicious npmFileMap");
-        hipchat.message('red', pkg.npmName+" has a malicious npmFileMap");
-        return callback(null);
+        hipchat.message('red', pkg.npmName+" has a malicious npmFileMap: "+ JSON.stringify(pkg.npmFileMap));
+        return cb(null);
     }
     console.log('Checking versions for ' + pkg.npmName);
     request.get('http://registry.npmjs.org/' + pkg.npmName, function(result) {
@@ -167,23 +201,40 @@ var updateLibrary = function (pkg, callback) {
             pkg.version = npmVersion;
             fs.writeFileSync('ajax/libs/' + pkg.name + '/package.json', JSON.stringify(pkg, null, 2), 'utf8');
 
-            callback(null);
+            cb(null);
         });
     });
 }
 
-console.log('Looking for npm enabled libraries...');
+exports.run = function(){
+    console.log('Looking for npm enabled libraries...');
 
-// load up those files
-var packages = glob.sync("./ajax/libs/*/package.json");
-packages = _(packages).map(function (pkg) {
-    var parsedPkg = parse(pkg);
-    return (parsedPkg.npmName && parsedPkg.npmFileMap) ? parsedPkg : null;
-}).compact().value();
-hipchat.message('green', 'Found ' + packages.length + ' npm enabled libraries');
-console.log('Found ' + packages.length + ' npm enabled libraries');
+    // load up those files
+    var packages = glob.sync("./ajax/libs/*/package.json");
+    packages = _(packages).map(function (pkg) {
+        var parsedPkg = parse(pkg);
+        return (parsedPkg.npmName && parsedPkg.npmFileMap) ? parsedPkg : null;
+    }).compact().value();
+    hipchat.message('green', 'Found ' + packages.length + ' npm enabled libraries');
+    console.log('Found ' + packages.length + ' npm enabled libraries');
 
-async.eachSeries(packages, updateLibrary, function(err) {
-  console.log('Script completed');
-  hipchat.message('green', 'Auto Update Completed - ' + newVersionCount + ' versions were updated');
-});
+    async.eachSeries(packages, updateLibrary, function(err) {
+      console.log('Script completed');
+      hipchat.message('green', 'Auto Update Completed - ' + newVersionCount + ' versions were updated');
+    });
+}
+exports.updateLibrary = updateLibrary;
+exports.updateLibraryVersion = updateLibraryVersion;
+exports.processNewVersion = processNewVersion;
+exports.error = error;
+exports.isAllowedPathFn = isAllowedPathFn;
+exports.isValidFileMap = isValidFileMap;
+
+
+var args = process.argv.slice(2);
+if(args.length > 0 && args[0] == 'run'){
+   exports.run()
+} else {
+   console.log('to start, pass the "run" arg')
+}
+
