7d22360c605d2a592b5b2228aa0f1d037957abad
Thomas Davis <thomasalwyndavis@gmail.com>
2014-04-30 19:45:44 -0400

    Updated packages via auto-update.js

---
+++ temp/loglevel/0.1.0/package/test/level-setting-test.js
@@ -0,0 +1,240 @@
+"use strict";
+
+var logMethods = [
+    "trace",
+    "debug",
+    "info",
+    "warn",
+    "error"
+];
+
+define(['../lib/loglevel'], function(log) {
+    var originalConsole = window.console;
+
+    describe("Log levels", function() {
+        beforeEach(function() {
+            window.console = {};
+
+            for (var ii = 0; ii < logMethods.length; ii++) {
+                window.console[logMethods[ii]] = jasmine.createSpy(logMethods[ii]);
+            }
+        });
+
+        afterEach(function() {
+            window.console = originalConsole;
+        });
+
+        describe("initial log level", function() {
+            it("disables all log methods", function() {
+                for (var ii = 0; ii < logMethods.length; ii++) {
+                    var method = logMethods[ii];
+                    log[method]("a log message");
+
+                    expect(console[method]).not.toHaveBeenCalled();
+                }
+            });
+        });
+
+        describe("log.enableAll()", function() {
+            it("enables all log methods", function() {
+                log.enableAll();
+
+                for (var ii = 0; ii < logMethods.length; ii++) {
+                    var method = logMethods[ii];
+                    log[method]("a log message");
+
+                    expect(console[method]).toHaveBeenCalled();
+                }
+            });
+        });
+
+        describe("invalid setLevel inputs", function() {
+            it("error thrown if no level is given", function() {
+                expect(function() {
+                    log.setLevel();
+                }).toThrow();
+            });
+
+            it("error thrown if null level is given", function() {
+                expect(function() {
+                    log.setLevel(null);
+                }).toThrow();
+            });
+
+            it("error thrown if undefined level is given", function() {
+                expect(function() {
+                    log.setLevel(undefined);
+                }).toThrow();
+            });
+
+            it("error thrown if invalid level number is given", function() {
+                expect(function() {
+                    log.setLevel(-1);
+                }).toThrow();
+            });
+
+            it("error thrown if invalid level name is given", function() {
+                expect(function() {
+                    log.setLevel("InvalidLevelName");
+                }).toThrow();
+            });
+        });
+
+        describe("setting log level by name", function() {
+            function itCanSetLogLevelTo(level) {
+                it("can set log level to " + level, function() {
+                    log.disableAll();
+                    log.setLevel(level);
+
+                    log[level]("log message");
+                    expect(console[level]).toHaveBeenCalled();
+                });
+            }
+
+            itCanSetLogLevelTo("trace");
+            itCanSetLogLevelTo("debug");
+            itCanSetLogLevelTo("info");
+            itCanSetLogLevelTo("warn");
+            itCanSetLogLevelTo("error");
+        });
+
+        describe("log level settings", function() {
+            describe("log.trace", function() {
+                it("is enabled at trace level", function() {
+                    log.setLevel(log.levels.TRACE);
+
+                    log.trace("a log message");
+                    expect(console.trace).toHaveBeenCalled();
+                });
+
+                it("is disabled at debug level", function() {
+                    log.setLevel(log.levels.DEBUG);
+
+                    log.trace("a log message");
+                    expect(console.trace).not.toHaveBeenCalled();
+                });
+
+                it("is disabled at silent level", function() {
+                    log.setLevel(log.levels.SILENT);
+
+                    log.trace("a log message");
+                    expect(console.trace).not.toHaveBeenCalled();
+                });
+            });
+
+            describe("log.debug", function() {
+                it("is enabled at trace level", function() {
+                    log.setLevel(log.levels.TRACE);
+
+                    log.debug("a log message");
+                    expect(console.debug).toHaveBeenCalled();
+                });
+
+                it("is enabled at debug level", function() {
+                    log.setLevel(log.levels.DEBUG);
+
+                    log.debug("a log message");
+                    expect(console.debug).toHaveBeenCalled();
+                });
+
+                it("is disabled at info level", function() {
+                    log.setLevel(log.levels.INFO);
+
+                    log.debug("a log message");
+                    expect(console.debug).not.toHaveBeenCalled();
+                });
+
+                it("is disabled at silent level", function() {
+                    log.setLevel(log.levels.SILENT);
+
+                    log.debug("a log message");
+                    expect(console.debug).not.toHaveBeenCalled();
+                });
+            });
+
+            describe("log.info", function() {
+                it("is enabled at debug level", function() {
+                    log.setLevel(log.levels.DEBUG);
+
+                    log.info("a log message");
+                    expect(console.info).toHaveBeenCalled();
+                });
+
+                it("is enabled at info level", function() {
+                    log.setLevel(log.levels.INFO);
+
+                    log.info("a log message");
+                    expect(console.info).toHaveBeenCalled();
+                });
+
+                it("is disabled at warn level", function() {
+                    log.setLevel(log.levels.WARN);
+
+                    log.info("a log message");
+                    expect(console.info).not.toHaveBeenCalled();
+                });
+
+                it("is disabled at silent level", function() {
+                    log.setLevel(log.levels.SILENT);
+
+                    log.info("a log message");
+                    expect(console.info).not.toHaveBeenCalled();
+                });
+            });
+
+            describe("log.warn", function() {
+                it("is enabled at info level", function() {
+                    log.setLevel(log.levels.INFO);
+
+                    log.warn("a log message");
+                    expect(console.warn).toHaveBeenCalled();
+                });
+
+                it("is enabled at warn level", function() {
+                    log.setLevel(log.levels.WARN);
+
+                    log.warn("a log message");
+                    expect(console.warn).toHaveBeenCalled();
+                });
+
+                it("is disabled at error level", function() {
+                    log.setLevel(log.levels.ERROR);
+
+                    log.warn("a log message");
+                    expect(console.warn).not.toHaveBeenCalled();
+                });
+
+                it("is disabled at silent level", function() {
+                    log.setLevel(log.levels.SILENT);
+
+                    log.warn("a log message");
+                    expect(console.warn).not.toHaveBeenCalled();
+                });
+            });
+
+            describe("log.error", function() {
+                it("is enabled at warn level", function() {
+                    log.setLevel(log.levels.WARN);
+
+                    log.error("a log message");
+                    expect(console.error).toHaveBeenCalled();
+                });
+
+                it("is enabled at error level", function() {
+                    log.setLevel(log.levels.ERROR);
+
+                    log.error("a log message");
+                    expect(console.error).toHaveBeenCalled();
+                });
+
+                it("is disabled at silent level", function() {
+                    log.setLevel(log.levels.SILENT);
+
+                    log.error("a log message");
+                    expect(console.error).not.toHaveBeenCalled();
+                });
+            });
+        });
+    });
+});
+
---
+++ temp/numeral.js/1.2.5/package/numeral.js
@@ -0,0 +1,251 @@
+
+// numeral.js
+// version : 1.2.6
+// author : Adam Draper
+// license : MIT
+// http://adamwdraper.github.com/Numeral-js/
+
+(function () {
+
+    /************************************
+        Constants
+    ************************************/
+
+    var numeral,
+        VERSION = '1.2.6',
+        round = Math.round, i,
+        // internal storage for language config files
+        languages = {},
+        currentLanguage = 'en',
+        // check for nodeJS
+        hasModule = (typeof module !== 'undefined' && module.exports);
+
+
+    /************************************
+        Constructors
+    ************************************/
+
+
+    // Numeral prototype object
+    function Numeral(number) {
+        this._n = number;
+    }
+
+    /**
+     * Implementation of toFixed() that treats floats more like decimals
+     *
+     * Fixes binary rounding issues (eg. (0.615).toFixed(2) === '0.61') that present
+     * problems for accounting- and finance-related software.
+     */
+    function toFixed (value, precision, optionals) {
+        var power = Math.pow(10, precision),
+            output;
+
+        // Multiply up by precision, round accurately, then divide and use native toFixed():
+        output = (Math.round(value * power) / power).toFixed(precision);
+
+        if (optionals) {
+            var optionalsRegExp = new RegExp('0{1,' + optionals + '}$');
+            output = output.replace(optionalsRegExp, '');
+        }
+
+        return output;
+    }
+
+    /************************************
+        Formatting
+    ************************************/
+
+    // determine what type of formatting we need to do
+    function formatNumeral (n, format) {
+        var output;
+        
+        // figure out what kind of format we are dealing with
+        if (format.indexOf('$') > -1) { // currency!!!!!
+            output = formatCurrency(n, format);
+        } else if (format.indexOf('%') > -1) { // percentage
+            output = formatPercentage(n, format);
+        } else if (format.indexOf(':') > -1) { // time
+            output = formatTime(n, format);
+        } else { // plain ol' numbers or bytes
+            output = formatNumber(n, format);
+        }
+
+        // return string
+        return output;
+    }
+
+    // revert to number
+    function unformatNumeral (n, string) {
+        if (string.indexOf(':') > -1) {
+            n._n = unformatTime(string);
+        } else {
+            if (languages[currentLanguage].delimiters.decimal !== '.') {
+                string = string.replace(/\./g,'').replace(languages[currentLanguage].delimiters.decimal, '.');
+            }
+            
+            // see if abbreviations are there so that we can multiply to the correct number
+            var thousandRegExp = new RegExp(languages[currentLanguage].abbreviations.thousand + '(?:\\)|\\' + languages[currentLanguage].currency.symbol + '?)$'),
+                millionRegExp = new RegExp(languages[currentLanguage].abbreviations.million + '(?:\\)|\\' + languages[currentLanguage].currency.symbol + '?)$');
+
+            // see if bytes are there so that we can multiply to the correct number
+            var prefixes = ['KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+                bytesMultiplier = false;
+
+            for (var power = 0; power <= prefixes.length; power++) {
+                bytesMultiplier = (string.indexOf(prefixes[power]) > -1) ? Math.pow(1024, power + 1) : false;
+                
+                if (bytesMultiplier) {
+                    break;
+                }
+            }
+
+            // do some math to create our number
+            n._n = ((bytesMultiplier) ? bytesMultiplier : 1) * ((string.match(thousandRegExp)) ? 1000 : 1) * ((string.match(millionRegExp)) ? 1000000 : 1) * ((string.indexOf('%') > -1) ? 0.01 : 1) * Number(((string.indexOf('(') > -1) ? '-' : '') + string.replace(/[^0-9\.'-]+/g, ''));
+        
+            // round if we are talking about bytes
+            n._n = (bytesMultiplier) ? Math.ceil(n._n) : n._n;
+        }
+        return n._n;
+    }
+
+    function formatCurrency (n, format) {
+        var prependSymbol = (format.indexOf('$') <= 1) ? true : false;
+
+        // remove $ for the moment
+        format = format.replace('$', '');
+
+        // format the number
+        var output = formatNumeral(n, format);
+
+        // position the symbol
+        if (prependSymbol) {
+            if (output.indexOf('(') > -1 || output.indexOf('-') > -1) {
+                output = output.split('');
+                output.splice(1, 0, languages[currentLanguage].currency.symbol);
+                output = output.join('');
+            } else {
+                output = languages[currentLanguage].currency.symbol + output;
+            }
+        } else {
+            if (output.indexOf(')') > -1) {
+                output = output.split('');
+                output.splice(-1, 0, languages[currentLanguage].currency.symbol);
+                output = output.join('');
+            } else {
+                output = output + languages[currentLanguage].currency.symbol;
+            }
+        }
+
+        return output;
+    }
+
+    function formatPercentage (n, format) {
+        format = format.replace('%', '');
+        n._n = n._n * 100;
+        var output = formatNumeral(n, format);
+        if (output.indexOf(')') > -1 ) {
+            output = output.split('');
+            output.splice(-1, 0, '%');
+            output = output.join('');
+        } else {
+            output = output + '%';
+        }
+        return output;
+    }
+
+    function formatTime (n, format) {
+        var hours = Math.floor(n._n/60/60),
+            minutes = Math.floor((n._n - (hours * 60 * 60))/60),
+            seconds = Math.round(n._n - (hours * 60 * 60) - (minutes * 60));
+        return hours + ':' + ((minutes < 10) ? '0' + minutes : minutes) + ':' + ((seconds < 10) ? '0' + seconds : seconds);
+    }
+
+    function unformatTime (string) {
+        var timeArray = string.split(':'),
+            seconds = 0;
+        // turn hours and minutes into seconds and add them all up
+        if (timeArray.length === 3) {
+            // hours
+            seconds = seconds + (Number(timeArray[0]) * 60 * 60);
+            // minutes
+            seconds = seconds + (Number(timeArray[1]) * 60);
+            // seconds
+            seconds = seconds + Number(timeArray[2]);
+        } else if (timeArray.lenght === 2) {
+            // minutes
+            seconds = seconds + (Number(timeArray[0]) * 60);
+            // seconds
+            seconds = seconds + Number(timeArray[1]);
+        }
+        return Number(seconds);
+    }
+
+    function formatNumber (n, format) {
+        var negP = false,
+            abbr = false,
+            bytes = false,
+            ord = false;
+
+        // see if we should use parentheses for negative number
+        if (format.indexOf('(') > -1) {
+            negP = true;
+            format = format.slice(1, -1);
+        }
+
+        // see if abbreviation is wanted
+        if (format.indexOf('a') > -1) {
+            format = format.replace('a', '');
+
+            if (n._n > 1000000) {
+                abbr = languages[currentLanguage].abbreviations.million;
+                n._n = n._n / 1000000;
+            } else {
+                abbr = languages[currentLanguage].abbreviations.thousand;
+                n._n = n._n / 1000;
+            }
+        }
+
+        // see if we are formatting bytes
+        if (format.indexOf('b') > -1) {
+            format = format.replace('b', '');
+
+            var prefixes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'],
+                min,
+                max;
+
+            for (var power = 0; power <= prefixes.length; power++) {
+                min = Math.pow(1024, power);
+                max = Math.pow(1024, power+1);
+
+                if (n._n > min && n._n < max) {
+                    bytes = prefixes[power];
+                    if (min > 0) {
+                        n._n = n._n / min;
+                    }
+                    break;
+                }
+            }
+        }
+
+        // see if ordinal is wanted
+        if (format.indexOf('o') > -1) {
+            format = format.replace('o', '');
+
+            ord = languages[currentLanguage].ordinal(n._n);
+        }
+
+        var w = n._n.toString().split('.')[0],
+            precision = format.split('.')[1],
+            thousands = format.indexOf(','),
+            d = '',
+            neg = false;
+
+        // format number
+        if (n._n < 0) {
+            w = w.slice(1);
+            neg = true;
+        }
+
+        if (thousands > -1) {
+            w = w.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1' + languages[currentLanguage].delimiters.thousands                                                                                     
