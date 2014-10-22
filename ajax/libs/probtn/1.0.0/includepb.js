c0250f8cbf93058d9a48fd087c9a0c4be4cdd59c
Pavel Khakhulin <p.hahulin@hintsolutions.ru>
2014-10-22 15:54:54 +0400

    more files and fix protocol

--- ajax/libs/probtn/1.0.0/includepb.js
+++ ajax/libs/probtn/1.0.0/includepb.js
@@ -1,6 +1,6 @@
 if (!window.jQuery) {
     console.log("No jquery");
-    document.write("<script src=\"http://code.jquery.com/jquery-1.9.1.js\"><\/script>");
+    document.write("<script src=\"//code.jquery.com/jquery-1.9.1.js\"><\/script>");
 } else {
     console.log("Is jquery");
 }
@@ -9,8 +9,8 @@ document.write(
     + "function ($) {"
         + "$.noConflict();"
         + "jQuery(document).ready(function() {"
-            + "jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/probtn/1.0.0/javascripts/jquery.pep.min.js', function() {"
-                + "jQuery.getScript('https://cdnjs.cloudflare.com/ajax/libs/probtn/1.0.0/javascripts/jquery.fancybox.js', function () {"
+            + "jQuery.getScript('//cdnjs.cloudflare.com/ajax/libs/probtn/1.0.0/javascripts/jquery.pep.min.js', function() {"
+                + "jQuery.getScript('//cdnjs.cloudflare.com/ajax/libs/probtn/1.0.0/javascripts/jquery.fancybox.js', function () {"
                     + "jQuery.getScript('https://pizzabtn.herokuapp.com/javascripts/probtn.js', function () {"
                         + "jQuery(document).ready(function($){$(document).StartButton({'mainStyleCss':'https://pizzabtn.herokuapp.com/stylesheets/probtn.css'});});"
                     + "})"
