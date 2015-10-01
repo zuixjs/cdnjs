37beb54bcc24d1dbe6d2d51ba1939c31b0e378b8
Peter Dave Hello <hsu@peterdavehello.org>
2015-10-01 21:54:00 +0800

    Disable SRI support and packages.min.json compress
    At this moment, we have memory usage issue.

--- build/packages.json.js
+++ build/packages.json.js
@@ -3,8 +3,6 @@
 var async = require('async');
 var glob = require('glob');
 var fs = require('fs');
-var lzma = require('lzma-native');
-var sriToolbox = require("sri-toolbox");
 var _ = require('underscore');
 var natcompare = require('./natcompare.js');
 var RSS = require('rss');
@@ -85,84 +83,64 @@ exec('git ls-tree -r --name-only HEAD | grep **/package.json | while read filena
 
 var packages = Array();
 
-fs.readFile('../new-website/public/packages.min.json.xz', function(err, raw) {
-  lzma.decompress(raw, function(data) {
-    data = JSON.parse(data);
-    glob("ajax/libs/*/package.json", function (error, matches) {
-      async.each(matches, function(item, callback) {
-        var package = JSON.parse(fs.readFileSync(item, 'utf8'));
-        delete package.main;
-        delete package.scripts;
-        delete package.bugs;
-        delete package.npmFileMap;
-        delete package.dependencies;
-        delete package.devDependencies;
-        if (package.npmName) {
-          package.autoupdate = 'npm';
-        } else if (package.autoupdate) {
-          package.autoupdate = package.autoupdate.source;
-        } else {
-          delete package.autoupdate;
+fs.readFile('../new-website/public/packages.min.json', 'utf8', function(err, data) {
+  data = JSON.parse(data);
+  glob("ajax/libs/*/package.json", function (error, matches) {
+    async.each(matches, function(item, callback) {
+      var package = JSON.parse(fs.readFileSync(item, 'utf8'));
+      delete package.main;
+      delete package.scripts;
+      delete package.bugs;
+      delete package.npmFileMap;
+      delete package.dependencies;
+      delete package.devDependencies;
+      if (package.npmName) {
+        package.autoupdate = 'npm';
+      } else if (package.autoupdate) {
+        package.autoupdate = package.autoupdate.source;
+      } else {
+        delete package.autoupdate;
+      }
+      package.assets = Array();
+      var oldVersions = Array();
+      var pkgSave;
+      data['packages'].forEach(function(pkg){
+        if (pkg.name == package.name) {
+          oldVersions = pkg['assets'].map(function(d){return d[['version']]});
+          pkgSave = pkg;
         }
-        package.assets = Array();
-        var oldVersions = Array();
-        var pkgSave;
-        data['packages'].forEach(function(pkg){
-          if (pkg.name == package.name) {
-            oldVersions = pkg['assets'].map(function(d){return d[['version']]});
-            pkgSave = pkg;
-          }
-        });
-        var versions = glob.sync("ajax/libs/"+package.name+"/!(package.json)/").map(function(ver){return ver.slice(0, -1);});
-        async.each(versions, function(version, callback) {
-          var temp = Object();
-          temp.version = version.replace(/^.+\//, "");
-          if (oldVersions.indexOf(temp.version) != -1) {
-            for (var i = 0, size = pkgSave['assets'].length; i < size; i ++) {
-              if ( pkgSave['assets'][i].version == temp.version) {
-                temp.files = pkgSave['assets'][i].files;
-              }
-            }
-          } else {
-            temp.files = glob.sync(version + "/**/*", {nodir:true});
-            for (var i = 0; i < temp.files.length; i++){
-              var filespec = temp.files[i];
-
-              var fileContent = fs.readFileSync(temp.files[i], { encoding: "ascii" });
-              var splitTemp = temp.files[i].split('.');
-              switch (splitTemp[splitTemp.length - 1]) {
-                case 'css':
-                case 'js':
-                  var integrity = sriToolbox.generate({algorithms: ["sha256"]}, fileContent);
-                  break;
-                default:
-                  var integrity = '';
-              }
-              delete fileContent;
-              delete splitTemp;
-              temp.files[i] = {
-                name: filespec.replace(version + "/", ""),
-                integrity: integrity
-              };
+      });
+      var versions = glob.sync("ajax/libs/"+package.name+"/!(package.json)/").map(function(ver){return ver.slice(0, -1);});
+      async.each(versions, function(version, callback) {
+        var temp = Object();
+        temp.version = version.replace(/^.+\//, "");
+        if (oldVersions.indexOf(temp.version) != -1) {
+          for (var i = 0, size = pkgSave['assets'].length; i < size; i ++) {
+            if ( pkgSave['assets'][i].version == temp.version) {
+              temp.files = pkgSave['assets'][i].files;
             }
           }
-          package.assets.push(temp);
-        }, function(err) {
-          console.log(err);
-        });
-        package.assets.sort(function(a, b){
-          return natcompare.compare(a.version, b.version);
-        })
-        package.assets.reverse();
-        packages.push(package);
+        } else {
+          temp.files = glob.sync(version + "/**/*", {nodir:true});
+          for (var i = 0; i < temp.files.length; i++){
+            var filespec = temp.files[i];
+            temp.files[i] = filespec.replace(version + "/", "");
+          }
+        }
+        package.assets.push(temp);
       }, function(err) {
         console.log(err);
       });
-      // Initialize the feed object
-      fs.writeFileSync('../cdnjs.debug.packages.json', JSON.stringify({"packages":packages}, null, 2), 'utf8');
-      lzma.compress(JSON.stringify({"packages":packages}), function(result) {
-        fs.writeFileSync('../new-website/public/packages.min.json.xz', result);
-      });
+      package.assets.sort(function(a, b){
+        return natcompare.compare(a.version, b.version);
+      })
+      package.assets.reverse();
+      packages.push(package);
+    }, function(err) {
+      console.log(err);
     });
+    // Initialize the feed object
+    fs.writeFileSync('../cdnjs.debug.packages.json', JSON.stringify({"packages":packages}, null, 2), 'utf8');
+    fs.writeFileSync('../new-website/public/packages.min.json', JSON.stringify({"packages":packages}), 'utf8');
   });
 });
