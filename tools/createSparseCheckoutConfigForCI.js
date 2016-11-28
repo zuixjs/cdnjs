18dbfda016cfc58100b777228218cd58a7183e5f
Peter Dave Hello <hsu@peterdavehello.org>
2016-11-29 04:14:10 +0800

    fix template bug in createSparseCheckoutConfigForCI.js

--- tools/createSparseCheckoutConfigForCI.js
+++ tools/createSparseCheckoutConfigForCI.js
@@ -10,7 +10,7 @@ var fs = require("fs"),
   glob = require("glob"),
   colors = require("colors"),
   basePath = "/ajax/libs/",
-  result = fs.readFileSync('tools/sparse-checkout.template'),
+  result = fs.readFileSync('tools/sparse-checkout-with-package.json.template'),
   packages = glob.sync("./ajax/libs/*/package.json");
 
 result += '/ajax/libs/*/package.json\n';
