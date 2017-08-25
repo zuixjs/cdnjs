e4d0b3e573912ddf3070e6874527d1b00260b620
Peter Dave Hello <hsu@peterdavehello.org>
2017-08-26 02:50:00 +0800

    Add old versions of inferno-component

---
+++ ajax/libs/inferno-component/1.3.0-rc.1/inferno-component.js
@@ -0,0 +1,15 @@
+
+/*!
+ * inferno-component v1.3.0-rc.1
+ * (c) 2017 Dominic Gannaway'
+ * Released under the MIT License.
+ */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('inferno')) :
+	typeof define === 'function' && define.amd ? define(['inferno'], factory) :
+	(global['inferno-component'] = factory(global.Inferno));
+}(this, (function (inferno) { 'use strict';
+
+var NO_OP = '$NO_OP';
+var ERROR_MSG = 'a runtime error occured! Use Inferno in deve
---
+++ ajax/libs/inferno-component/1.5.4/inferno-component.js
@@ -0,0 +1,328 @@
+
+/*!
+ * Inferno.Component v1.5.4
+ * (c) 2017 Dominic Gannaway'
+ * Released under the MIT License.
+ */
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('inferno')) :
+	typeof define === 'function' && define.amd ? define(['inferno'], factory) :
+	(global.Inferno = global.Inferno || {}, global.Inferno.Component = factory(global.Inferno));
+}(this, (function (inferno) { 'use strict';
+
+var NO_OP = '$NO_OP';
+var ERROR_MSG = 'a runtime error occured! Use Inferno in development environment to find the error.';
+// This should be boolean and not reference to window.document
+var isBrowser = !!(typeof window !== 'undefined' && window.document);
+
+// this is MUCH faster than .constructor === Array and instanceof Array
+// in Node 7 and the later versions of V8, slower in older versions though
+var isArray = Array.isArray;
+
+function isStringOrNumber(obj) {
+    var type = typeof obj;
+    return type === 'string' || type === 'number';
+}
+function isNullOrUndef(obj) {
+    return isUndefined(obj) || isNull(obj);
+}
+function isInvalid(obj) {
+    return isNull(obj) || obj === false || isTrue(obj) || isUndefined(obj);
+}
+function isFunction(obj) {
+    return typeof obj === 'function';
+}
+
+
+function isNull(obj) {
+    return obj === null;
+}
+function isTrue(obj) {
+    return obj === true;
+}
+function isUndefined(obj) {
+    return obj === undefined;
+}
+
+function throwError(message) {
+    if (!message) {
+        message = ERROR_MSG;
+    }
+    throw new Error(("Inferno Error: " + message));
+}
+
+function combineFrom(first, second) {
+    var obj = {};
+    var key;
+    if (first) {
+        for (key in first) {
+            obj[key] = first[key];
+        }
+    }
+    if (second) {
+        for (key in second) {
+            obj[key] = second[key];
+        }
+    }
+    return obj;
+}
+function Lifecycle() {
+    this.listeners = [];
+}
+Lifecycle.prototype.addListener = function addListener(callback) {
+    this.listeners.push(callback);
+};
+Lifecycle.prototype.trigger = function trigger() {
+    var listeners = this.listeners;
+    for (var i = 0, len = listeners.length; i < len; i++) {
+        listeners[i]();
+    }
+};
+
+// Make sure u use EMPTY_OBJ from 'inferno', otherwise it'll be a different reference
+var noOp = ERROR_MSG;
+{
+    noOp = 'Inferno Error: Can only update a mounted or mounting component. This usually means you called setState() or forceUpdate() on an unmounted component. This is a no-op.';
+}
+var componentCallbackQueue = new Map();
+// when a components root VNode is also a component, we can run into issues
+// this will recursively look for vNode.parentNode if the VNode is a component
+function updateParentComponentVNodes(vNode, dom) {
+    if (vNode.flags & 28 /* Component */) {
+        var parentVNode = vNode.parentVNode;
+        if (parentVNode) {
+            parentVNode.dom = dom;
+            updateParentComponentVNodes(parentVNode, dom);
+        }
+    }
+}
+var resolvedPromise = Promise.resolve();
+function addToQueue(component, force, callback) {
+    // TODO this function needs to be revised and improved on
+    var queue = componentCallbackQueue.get(component);
+    if (!queue) {
+        queue = [];
+        componentCallbackQueue.set(component, queue);
+        resolvedPromise.then(function () {
+            componentCallbackQueue.delete(component);
+            component._updating = true;
+            applyState(component, force, function () {
+                for (var i = 0, len = queue.length; i < len; i++) {
+                    queue[i]();
+                }
+            });
+            component._updating = false;
+        });
+    }
+    if (callback) {
+        queue.push(callback);
+    }
+}
+function queueStateChanges(component, newState, callback, sync) {
+    if (isFunction(newState)) {
+        newState = newState(component.state, component.props, component.context);
+    }
+    for (var stateKey in newState) {
+        component._pendingState[stateKey] = newState[stateKey];
+    }
+    if (!component._pendingSetState && isBrowser && !(sync && component._blockRender)) {
+        if ((sync || component._blockRender) && !component._updating) {
+            component._pendingSetState = true;
+            component._updating = true;
+            applyState(component, false, callback);
+            component._updating = false;
+        }
+        else {
+            addToQueue(component, false, callback);
+        }
+    }
+    else {
+        var pending = component._pendingState;
+        var state = component.state;
+        for (var key in pending) {
+            state[key] = pending[key];
+        }
+        component._pendingState = {};
+    }
+}
+function applyState(component, force, callback) {
+    if ((!component._deferSetState || force) && !component._blockRender && !component._unmounted) {
+        component._pendingSetState = false;
+        var pendingState = component._pendingState;
+        var prevState = component.state;
+        var nextState = combineFrom(prevState, pendingState);
+        var props = component.props;
+        var context = component.context;
+        component._pendingState = {};
+        var nextInput = component._updateComponent(prevState, nextState, props, props, context, force, true);
+        var didUpdate = true;
+        if (isInvalid(nextInput)) {
+            nextInput = inferno.createVNode(4096 /* Void */);
+        }
+        else if (nextInput === NO_OP) {
+            nextInput = component._lastInput;
+            didUpdate = false;
+        }
+        else if (isStringOrNumber(nextInput)) {
+            nextInput = inferno.createVNode(1 /* Text */, null, null, nextInput);
+        }
+        else if (isArray(nextInput)) {
+            {
+                throwError('a valid Inferno VNode (or null) must be returned from a component render. You may have returned an array or an invalid object.');
+            }
+            throwError();
+        }
+        var lastInput = component._lastInput;
+        var vNode = component._vNode;
+        var parentDom = (lastInput.dom && lastInput.dom.parentNode) || (lastInput.dom = vNode.dom);
+        component._lastInput = nextInput;
+        if (didUpdate) {
+            var childContext, subLifecycle = component._lifecycle;
+            if (!subLifecycle) {
+                subLifecycle = new Lifecycle();
+            }
+            else {
+                subLifecycle.listeners = [];
+            }
+            component._lifecycle = subLifecycle;
+            if (!isUndefined(component.getChildContext)) {
+                childContext = component.getChildContext();
+            }
+            if (isNullOrUndef(childContext)) {
+                childContext = component._childContext;
+            }
+            else {
+                childContext = combineFrom(context, childContext);
+            }
+            component._patch(lastInput, nextInput, parentDom, subLifecycle, childContext, component._isSVG, false);
+            subLifecycle.trigger();
+            if (!isUndefined(component.componentDidUpdate)) {
+                component.componentDidUpdate(props, prevState, context);
+            }
+            inferno.options.afterUpdate && inferno.options.afterUpdate(vNode);
+        }
+        var dom = vNode.dom = nextInput.dom;
+        var componentToDOMNodeMap = component._componentToDOMNodeMap;
+        componentToDOMNodeMap && componentToDOMNodeMap.set(component, nextInput.dom);
+        updateParentComponentVNodes(vNode, dom);
+        if (!isNullOrUndef(callback)) {
+            callback();
+        }
+    }
+    else if (!isNullOrUndef(callback)) {
+        if (component._blockRender) {
+            component.state = component._pendingState;
+            component._pendingState = {};
+        }
+        callback();
+    }
+}
+var Component = function Component(props, context) {
+    this.state = {};
+    this._blockRender = false;
+    this._ignoreSetState = false;
+    this._blockSetState = false;
+    this._deferSetState = false;
+    this._pendingSetState = false;
+    this._pendingState = {};
+    this._lastInput = null;
+    this._vNode = null;
+    this._unmounted = false;
+    this._lifecycle = null;
+    this._childContext = null;
+    this._patch = null;
+    this._isSVG = false;
+    this._componentToDOMNodeMap = null;
+    this._updating = false;
+    /** @type {object} */
+    this.props = props || inferno.EMPTY_OBJ;
+    /** @type {object} */
+    this.context = context || inferno.EMPTY_OBJ; // context should not be mutable
+};
+Component.prototype.render = function render (nextProps, nextState, nextContext) { };
+Component.prototype.forceUpdate = function forceUpdate (callback) {
+    if (this._unmounted) {
+        return;
+    }
+    isBrowser && applyState(this, true, callback);
+};
+Component.prototype.setState = function setState (newState, callback) {
+    if (this._unmounted) {
+        return;
+    }
+    if (!this._blockSetState) {
+        if (!this._ignoreSetState) {
+            queueStateChanges(this, newState, callback, false);
+        }
+    }
+    else {
+        {
+            throwError('cannot update state via setState() in componentWillUpdate().');
+        }
+        throwError();
+    }
+};
+Component.prototype.setStateSync = function setStateSync (newState) {
+    if (this._unmounted) {
+        return;
+    }
+    if (!this._blockSetState) {
+        if (!this._ignoreSetState) {
+            queueStateChanges(this, newState, null, true);
+        }
+    }
+    else {
+        {
+            throwError('cannot update state via setState() in componentWillUpdate().');
+        }
+        throwError();
+    }
+};
+Component.prototype._updateComponent = function _updateComponent (prevState, nextState, prevProps, nextProps, context, force, fromSetState) {
+    if (this._unmounted === true) {
+        {
+            throwError(noOp);
+        }
+        throwError();
+    }
+    if ((prevProps !== nextProps || nextProps === inferno.EMPTY_OBJ) || prevState !== nextState || force) {
+        if (prevProps !== nextProps || nextProps === inferno.EMPTY_OBJ) {
+            if (!isUndefined(this.componentWillReceiveProps) && !fromSetState) {
+                this._blockRender = true;
+                this.componentWillReceiveProps(nextProps, context);
+                this._blockRender = false;
+            }
+            if (this._pendingSetState) {
+                nextState = combineFrom(nextState, this._pendingState);
+                this._pendingSetState = false;
+                this._pendingState = {};
+            }
+        }
+        /* Update if scu is not defined, or it returns truthy value or force */
+        if (isUndefined(this.shouldComponentUpdate) || this.shouldComponentUpdate(nextProps, nextState, context) || force) {
+            if (!isUndefined(this.componentWillUpdate)) {
+                this._blockSetState = true;
+                this.componentWillUpdate(nextProps, nextState, context);
+                this._blockSetState = false;
+            }
+            this.props = nextProps;
+            this.state = nextState;
+            this.context = context;
+            inferno.options.beforeRender && inferno.options.beforeRender(this);
+            var render = this.render(nextProps, nextState, context);
+            inferno.options.afterRender && inferno.options.afterRender(this);
+            return render;
+        }
+        else {
+            this.props = nextProps;
+            this.state = nextState;
+            this.context = context;
+        }
+    }
+    return NO_OP;
+};
+
+return Component;
+
+})));
