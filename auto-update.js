b9deefd67ef508b5421dbe93ff12d71f3a5d48da
Peter Dave Hello <hsu@peterdavehello.org>
2017-09-15 22:34:31 +0800

    Fix auto-update clean up

--- auto-update.js
+++ auto-update.js
@@ -18,7 +18,6 @@ var args;
 
 if (fs.existsSync('/run/shm')) {
   tempDirPath = '/run/shm/' + process.env.USER + '/cdnjs_NPM_temp';
-  fs.mkdirsSync(tempDirPath);
 } else {
   tempDirPath = path.join(__dirname, 'temp');
 }
@@ -319,7 +318,8 @@ var updateLibrary = function (pkg, cb) {
 };
 
 exports.run = function () {
-  fs.removeSync(path.join(tempDirPath, '/*'));
+  fs.removeSync(path.join(tempDirPath));
+  fs.mkdirsSync(tempDirPath);
 
   console.log('Looking for npm enabled libraries...');
 
@@ -342,7 +342,6 @@ exports.run = function () {
     var msg = 'Auto Update Completed - ' + newVersionCount +
       ' versions were updated';
     console.log(msg.prompt);
-    fs.removeSync(path.join(tempDirPath, '/*'));
     if (err) {
       console.dir(err);
     }
