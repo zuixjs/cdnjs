aead310892126342f17c593d1c00347a314cf713
PeterBot <PeterBot@users.noreply.github.com>
2018-01-03 19:58:15 +0800

    Add qoopido.nucleus v3.0.6

---
+++ ajax/libs/qoopido.nucleus/3.0.6/function/debounce.js
@@ -0,0 +1,2 @@
+!function(n){"use strict";function t(){return function(t,c,e){function i(){function r(){i.cancel(),!e&&t.apply(a,o)}var a=this,o=arguments,f=e&&!u;i.cancel(),u=n(r,parseInt(c,10)||200),f&&t.apply(a,o)}var u;return i.cancel=function(){u=clearTimeout(u)},i}}provide(t)}(setTimeout);
+//# sourceMappingURL=debounce.js.map
