ed5fcf47450aeea41fd8bd167f34e15281c9bcc9
PeterBot <PeterBot@users.noreply.github.com>
2018-01-16 00:31:25 +0800

    Add Kraken v7.6.0

---
+++ ajax/libs/Kraken/7.6.0/js/main.js
@@ -0,0 +1,9 @@
+/*!
+ * Kraken v7.6.0: A lightweight front-end boilerplate
+ * (c) 2018 Chris Ferdinandi
+ * MIT License
+ * http://github.com/cferdinandi/kraken
+ * Open Source Credits: 
+ */
+
+// Add your scripts here...
