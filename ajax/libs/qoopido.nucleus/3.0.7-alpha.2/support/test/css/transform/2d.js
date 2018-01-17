33042267471c247ef500ca45322366d50e332fc5
PeterBot <PeterBot@users.noreply.github.com>
2018-01-18 00:26:15 +0800

    Add qoopido.nucleus v3.0.7-alpha.2

---
+++ ajax/libs/qoopido.nucleus/3.0.7-alpha.2/support/test/css/transform/2d.js
@@ -0,0 +1,2 @@
+!function(e){"use strict";provide(["/demand/pledge","../transform"],function(t,r){var n=t.defer();return r.then(function(t){var r=e.createElement("div").style;try{r[t]="rotate(30deg)"}catch(e){}/rotate/.test(r[t])?n.resolve():n.reject()},n.reject),n.pledge})}(document);
+//# sourceMappingURL=2d.js.map
---
+++ ajax/libs/qoopido.nucleus/3.0.7-alpha.2/support/test/element/video.js
@@ -0,0 +1,2 @@
+!function(e){"use strict";provide(["/demand/pledge"],function(n){var r=n.defer();return"canPlayType"in e.createElement("video")?r.resolve():r.reject(),r.pledge})}(document);
+//# sourceMappingURL=video.js.map
