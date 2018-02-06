d296c50055dc72263325f852bc251ec86e2b4fe1
PeterBot <PeterBot@users.noreply.github.com>
2018-02-06 19:27:34 +0800

    Add qoopido.nucleus v3.0.7

---
+++ ajax/libs/qoopido.nucleus/3.0.7/support/test/css/boxshadow.js
@@ -0,0 +1,2 @@
+!function(){"use strict";provide(["/demand/pledge","../../css/property"],function(e,r){var d=e.defer(),o=r("box-shadow");return o?d.resolve(o):d.reject(),d.pledge})}();
+//# sourceMappingURL=boxshadow.js.map
---
+++ ajax/libs/qoopido.nucleus/3.0.7/support/test/css/rem.js
@@ -0,0 +1,2 @@
+!function(e){"use strict";provide(["/demand/pledge"],function(t){var r=t.defer(),n=e.createElement("div").style;try{n.fontSize="3rem"}catch(e){}return/rem/.test(n.fontSize)?r.resolve():r.reject(),r.pledge})}(document);
+//# sourceMappingURL=rem.js.map
