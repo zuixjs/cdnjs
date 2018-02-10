614119a87ebd239c3467597688b76d791c0b4d60
PeterBot <PeterBot@users.noreply.github.com>
2018-02-10 08:30:10 +0800

    Add qoopido.nucleus v3.1.0-alpha.3

---
+++ ajax/libs/qoopido.nucleus/3.1.0-alpha.3/support/test/element/canvas/todataurl/webp.js
@@ -0,0 +1,2 @@
+!function(e){"use strict";provide(["/demand/pledge","../todataurl"],function(t,a){var n=t.defer();return a.then(function(){0===e.createElement("canvas").toDataURL("image/webp").indexOf("data:image/webp")?n.resolve():n.reject()},n.reject),n.pledge})}(document);
+//# sourceMappingURL=webp.js.map
