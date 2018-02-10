112210bd9bf25ab3d51aed0e9463125a7ff7115e
PeterBot <PeterBot@users.noreply.github.com>
2018-02-10 09:11:58 +0800

    Add qoopido.nucleus v3.1.0-alpha.6

---
+++ ajax/libs/qoopido.nucleus/3.1.0-alpha.6/component/sense.js
@@ -0,0 +1,2 @@
+!function(t){"use strict";provide(["/demand/weakmap","../emitter",t.matchMedia||"../polyfill/window/matchmedia"],function(t,e,a){var i=new t;function n(t){var n=e.call(this),r=a(t);return i.set(n,r),r.addListener(function(){n.emit(!0===r.matches?"match":"unmatch")}),n}return n.prototype={get matches(){var t=i.get(this);if(t)return t.matches}},n.extends(e)})}(this);
+//# sourceMappingURL=sense.js.map
---
+++ ajax/libs/qoopido.nucleus/3.1.0-alpha.6/support/test/css/rgba.js
@@ -0,0 +1,2 @@
+!function(e){"use strict";provide(["/demand/pledge"],function(r){var t=r.defer(),c=e.createElement("div").style,d="backgroundColor";try{c[d]="rgba(0,0,0,.5)"}catch(e){}return/rgba/.test(c[d])?t.resolve():t.reject(),t.pledge})}(document);
+//# sourceMappingURL=rgba.js.map
