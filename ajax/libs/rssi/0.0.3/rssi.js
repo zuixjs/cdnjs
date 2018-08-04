1a51a104c9930689d201a02b267b3d213c85eeb5
PeterBot <PeterBot@users.noreply.github.com>
2018-08-04 12:31:24 +0800

    Add rssi v0.0.3

---
+++ ajax/libs/rssi/0.0.3/rssi.js
@@ -0,0 +1,14 @@
+(function () {
+    function rewrite(foo, bar) {
+        return '"+(typeof obj["' + bar + '"]!="undefined"?obj["' + bar + '"]:"' + foo + '")+"'
+    }
+
+    function fmt(input) {
+        var out = JSON.stringify(input).replace(/#\{(.*?)\}/g, rewrite)
+        /* jshint evil: true */
+        return Function('obj', 'return ' + out)
+    }
+
+    if (typeof module != 'undefined' && module.exports) module.exports = fmt
+    else if (typeof define == 'function' && define.amd) define(function () { return fmt })
+}())
