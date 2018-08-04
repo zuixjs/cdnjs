b8dad4ed283febdabb7e9b21b128a84b5957f823
PeterBot <PeterBot@users.noreply.github.com>
2018-08-04 12:30:33 +0800

    Add rssi v0.0.5

---
+++ ajax/libs/rssi/0.0.5/rssi.js
@@ -0,0 +1,22 @@
+/* Ruby-like simple string interpolation for Node.js
+ * Copyright (c) 2013 Mark Vasilkov (https://github.com/mvasilkov)
+ * License: MIT */
+(function () {
+    var cache = {}
+
+    function rewrite(foo, bar) {
+        return '"+(typeof obj["' + bar + '"]!="undefined"?obj["' + bar + '"]:"' + foo + '")+"'
+    }
+
+    function fmt(input, options) {
+        if (!(options && options.noCache) && input in cache) return cache[input]
+
+        var out = JSON.stringify(input).replace(/#\{(.*?)\}/g, rewrite)
+        /* jshint boss: true, evil: true */
+        return (cache[input] = Function('obj', 'return ' + out))
+    }
+
+    if (typeof module != 'undefined' && module.exports) module.exports = fmt
+    else if (typeof define == 'function' && define.amd) define(function () { return fmt })
+    else if (typeof window != 'undefined') window.fmt = fmt
+}())
