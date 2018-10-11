20ed0a8f3360a5cba2e3f01d9214f73ffd15e1b3
PeterBot <PeterBot@users.noreply.github.com>
2018-10-11 23:48:39 +0800

    Add qoopido.nucleus v3.1.5-rc.3

---
+++ ajax/libs/qoopido.nucleus/3.1.5-rc.3/support/test/css/borderradius.js
@@ -0,0 +1,2 @@
+!function(){"use strict";provide(["/demand/pledge","../../css/property"],function(e,r){var d=e.defer(),t=r("border-radius");return t?d.resolve(t):d.reject(),d.pledge})}();
+//# sourceMappingURL=borderradius.js.map
